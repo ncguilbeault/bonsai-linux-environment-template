1b1d2ded0bbd10084d8f8a636c0e0255759e042a
ncguilbeault <ncguilbeault@gmail.com>
2024-05-20 23:04:18 +0100

    Updated scripts to remove cd command

--- Bonsai.LinuxEnvironmentTemplate/Setup.sh
+++ Bonsai.LinuxEnvironmentTemplate/Setup.sh
@@ -1,25 +1,22 @@
 #! /bin/bash
 
-CURRENT_DIR="$(pwd)"
 SETUP_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-cd $SETUP_SCRIPT_DIR
 
-if [ ! -f "./Bonsai.exe" ]; then
-    CONFIG="./Bonsai.config"
+if [ ! -f "$SETUP_SCRIPT_DIR/Bonsai.exe" ]; then
+    CONFIG="$SETUP_SCRIPT_DIR/Bonsai.config"
     if [ -f "$CONFIG" ]; then
         VERSION=$(xmllint --xpath '//PackageConfiguration/Packages/Package[@id="Bonsai"]/@version' "$CONFIG" | sed -e 's/^[^"]*"//' -e 's/"$//')
         RELEASE="https://github.com/bonsai-rx/bonsai/releases/download/$VERSION/Bonsai.zip"
     else
         RELEASE="https://github.com/bonsai-rx/bonsai/releases/latest/download/Bonsai.zip"
     fi
-    wget $RELEASE -O "temp.zip"
-    mv -f "NuGet.config" "temp.config"
-    unzip -d "." -o "temp.zip"
-    mv -f "temp.config" "NuGet.config"
-    rm -rf "temp.zip"
-    rm -rf "Bonsai32.exe"
+    wget $RELEASE -O "$SETUP_SCRIPT_DIR/temp.zip"
+    mv -f "$SETUP_SCRIPT_DIR/NuGet.config" "$SETUP_SCRIPT_DIR/temp.config"
+    unzip -d "$SETUP_SCRIPT_DIR" -o "$SETUP_SCRIPT_DIR/temp.zip"
+    mv -f "$SETUP_SCRIPT_DIR/temp.config" "$SETUP_SCRIPT_DIR/NuGet.config"
+    rm -rf "$SETUP_SCRIPT_DIR/temp.zip"
+    rm -rf "$SETUP_SCRIPT_DIR/Bonsai32.exe"
 fi
 
-source ./activate
-source ./run --no-editor
-cd $CURRENT_DIR
+source "$SETUP_SCRIPT_DIR/activate"
+source "$SETUP_SCRIPT_DIR/run" --no-editor
